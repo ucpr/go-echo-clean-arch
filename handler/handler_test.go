a874d5b2e36be0d166145a65c940b5237d387337
ucpr <hoge.uchihara@gmail.com>
2019-05-14 00:54:53 +0900

    feat: update test

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -11,48 +11,19 @@ import (
 )
 
 const (
-	test_json_data1 = `{"text":"HelloWorld!"}`
-	test_form_data = `text=HelloWorld!`
-	test_json_data2 = `{"txt":"hoge"}`
+	testJsonBody = `{"text":"HelloWorld!"}`
 )
 
 func TestIndexHandler(t *testing.T) {
 	e := echo.New()
 
-	// post json data
-
-	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(test_json_data1))
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(testJsonBody))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
 	if assert.NoError(t, IndexHandler(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, test_json_data1, strings.TrimRight(rec.Body.String(), "\n"))
+		assert.Equal(t, testJsonBody, strings.TrimRight(rec.Body.String(), "\n"))
 	}
-
-	// post form data
-
-	req = httptest.NewRequest(http.MethodPost, "/", strings.NewReader(test_form_data))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
-	rec = httptest.NewRecorder()
-	c = e.NewContext(req, rec)
-
-	if assert.NoError(t, IndexHandler(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, test_json_data1, strings.TrimRight(rec.Body.String(), "\n"))
-	}
-
-	// post 
-
-	req = httptest.NewRequest(http.MethodPost, "/", strings.NewReader(test_form_data))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
-	rec = httptest.NewRecorder()
-	c = e.NewContext(req, rec)
-
-	if assert.NoError(t, IndexHandler(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, test_json_data1, strings.TrimRight(rec.Body.String(), "\n"))
-	}
-
 }
